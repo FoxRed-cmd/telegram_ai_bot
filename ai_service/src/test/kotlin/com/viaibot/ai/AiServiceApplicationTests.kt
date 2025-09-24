bbe90ff816b26691c6dd3ecd503755bd129af580
Blinov Kirill <blinovk955@gmail.com>
2025-09-24 17:17:20 +0300

    init project

---
+++ ai_service/src/test/kotlin/com/viaibot/ai/AiServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.viaibot.ai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AiServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
